0e1a0dd62872f91edba4ec7c13b54441fda4339c
Silvana Gjini <silvanagjini@gmail.com>
2022-05-31 20:30:57 -0400

    final code week 3

--- week3/.history/server_20220516090920.js
+++
@@ -1,9 +0,0 @@
-const http =require('http'); //commonJS module pattern (old method)
-const fs =require('fs');
-
-const server=http.createServer(function(req, res)
-{
-    console.log(__dirname);
-//fs.readFile(__dirname + req.url)
-}
-)
--- week3/app.ts
+++ week3/app.ts
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors';
+import express, { NextFunction } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-var indexRouter = require('./Routes/index');
-var usersRouter = require('./Routes/users');
+import indexRouter from './Routes/index';
+//import usersRouter from './Routes/users';
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'Views'));
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'Public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err: createError.HttpError, req: express.Request, res: express.Response, next: NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +38,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
