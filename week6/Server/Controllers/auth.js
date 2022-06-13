d11e79faf6ed2b593716ea9e4644aca0a617bd5c
Silvana Gjini <silvanagjini@gmail.com>
2022-06-13 10:57:12 -0400

    completed auth functions

--- week6/Server/Controllers/auth.js
+++ week6/Server/Controllers/auth.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ProcessLogoutPage = exports.ProcessRegisterPage = exports.ProcessLoginPage = exports.DisplayRegisterPage = exports.DisplayLoginPage = void 0;
 //need passport functionality
 const passport_1 = __importDefault(require("passport"));
+//Include the User model for authentication function
+const user_1 = __importDefault(require("../Models/user"));
 function DisplayLoginPage(req, res, next) {
     res.render('index', { title: 'Login', page: 'login', messages: req.flash('loginMessage'), displayName: '' });
 }
@@ -40,11 +42,35 @@ function ProcessLoginPage(req, res, next) {
 }
 exports.ProcessLoginPage = ProcessLoginPage;
 function ProcessRegisterPage(req, res, next) {
-    res.render('index', { title: 'Services', page: 'services' });
+    // instantiate a new user object
+    let newUser = new user_1.default({
+        username: req.body.username,
+        EmailAddress: req.body.emailAddress,
+        DisplayName: req.body.firstName + " " + req.body.lastName
+    });
+    user_1.default.register(newUser, req.body.password, function (err) {
+        if (err) {
+            if (err.name == "UserExistsError") {
+                console.error('ERROR: User Already Exists!');
+                req.flash('registerMessage', 'Registration Error!');
+            }
+            else {
+                console.error(err.name); // other error
+                req.flash('registerMessage', 'Server Error');
+            }
+            return res.redirect('/register');
+        }
+        // everything is ok - user has been registered
+        // automatically login the user
+        return passport_1.default.authenticate('local')(req, res, function () {
+            return res.redirect('/movie-list');
+        });
+    });
 }
 exports.ProcessRegisterPage = ProcessRegisterPage;
 function ProcessLogoutPage(req, res, next) {
-    res.render('index', { title: 'Contact', page: 'contact' });
+    req.logOut();
+    res.redirect('/login');
 }
 exports.ProcessLogoutPage = ProcessLogoutPage;
 //# sourceMappingURL=auth.js.map
