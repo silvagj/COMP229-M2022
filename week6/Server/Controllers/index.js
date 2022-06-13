0f6264fd90a9dfc3a5d600dae308d1468f225a5c
Silvana Gjini <silvanagjini@gmail.com>
2022-06-13 10:27:01 -0400

    added configuration for authentication pages

--- week6/Server/Controllers/index.js
+++ week6/Server/Controllers/index.js
@@ -2,23 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DisplayContactsPage = exports.DisplayServicePage = exports.DisplayProjectPage = exports.DisplayAboutPage = exports.DisplayHomePage = void 0;
 function DisplayHomePage(req, res, next) {
-    res.render('index', { title: 'Home', page: 'home' });
+    res.render('index', { title: 'Home', page: 'home', displayName: '' });
 }
 exports.DisplayHomePage = DisplayHomePage;
 function DisplayAboutPage(req, res, next) {
-    res.render('index', { title: 'About Us', page: 'about' });
+    res.render('index', { title: 'About Us', page: 'about', displayName: '' });
 }
 exports.DisplayAboutPage = DisplayAboutPage;
 function DisplayProjectPage(req, res, next) {
-    res.render('index', { title: 'My Projects', page: 'projects' });
+    res.render('index', { title: 'My Projects', page: 'projects', displayName: '' });
 }
 exports.DisplayProjectPage = DisplayProjectPage;
 function DisplayServicePage(req, res, next) {
-    res.render('index', { title: 'Services', page: 'services' });
+    res.render('index', { title: 'Services', page: 'services', displayName: '' });
 }
 exports.DisplayServicePage = DisplayServicePage;
 function DisplayContactsPage(req, res, next) {
-    res.render('index', { title: 'Contact', page: 'contact' });
+    res.render('index', { title: 'Contact', page: 'contact', displayName: '' });
 }
 exports.DisplayContactsPage = DisplayContactsPage;
 //# sourceMappingURL=index.js.map
