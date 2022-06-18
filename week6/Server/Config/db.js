111c9911a87fab76d82d470e3b307014d69dc170
Silvana Gjini <silvanagjini@gmail.com>
2022-06-18 14:22:16 -0400

    added edit page and updated client-list

--- week6/Server/Config/db.js
+++ week6/Server/Config/db.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://127.0.0.1/media";
 exports.RemoteURI = "mongodb+srv://silvana:T6gZkz5dKGWnvBzb@cluster0.remhq.mongodb.net/media?retryWrites=true&w=majority";
+//export const RemoteURI = process.env.RemoteURI;
 exports.HostName = "MongoDB Atlas";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
